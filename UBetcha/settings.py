ec793b6d66649e65eb25beeea520e8636372a0e8
JohnMartin712 <35937340+JohnMartin712@users.noreply.github.com>
2019-08-11 18:21:29 -0400

    Update settings.py

--- UBetcha/settings.py
+++ UBetcha/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+od%6n%gumclv&2%#0jgj)y-2rw!13xf#35!*g+oy6plr618(u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['165.22.3.60']
+ALLOWED_HOSTS = []
 
 
 # Application definition
